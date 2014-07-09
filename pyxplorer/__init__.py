48f94215005718a0acbe4fafe5ceee68a2436b47
grundprinzip <grundprinzip@gmail.com>
2014-07-09 14:16:48 +0200

    fixing bugs in registration

---
+++ pyxplorer/__init__.py
@@ -0,0 +1,9 @@
+
+# The database helper
+from manager import Database
+
+# Relevant for getting Table information
+from types import Column, Table
+
+# The HDFS Loader
+from loader import Loader
--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ setup(
     name='pyxplorer',
     version='0.1.0',
     author='Martin Grund',
-    author_email='grundprinzip+piphgmail.com',
+    author_email='grundprinzip+pip@gmail.com',
     packages=['pyxplorer'],
     url='http://github.com/grundprinzip/pyxplorer',
     license='LICENSE',
-    description='Useful towel-related stuff.',
+    description='Simple Big Data Profiling',
     long_description=open('README.md').read(),
     install_requires=[
         "snakebite",
